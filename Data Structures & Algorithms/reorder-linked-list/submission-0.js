e5149fa9362cdccda2bf41ba8af16e16e2bbbb7f
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-17 16:43:24 -0400

    Add: reorder-linked-list - submission-0

---
+++ Data Structures & Algorithms/reorder-linked-list/submission-0.js
@@ -0,0 +1,50 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {void}
+     */
+    reorderList(head) {
+        let slow = head;
+        let fast = head;
+        const stack = [];
+        while (fast.next && fast.next.next) {
+            stack.push(slow);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        let prev = null;
+        let curr = slow.next;
+        slow.next = null;
+
+        while (curr !== null) {
+            const nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+
+        let first = head;
+        let second = prev;
+        while (second !== null) {
+            const temp1 = first.next;
+            const temp2 = second.next;
+
+            first.next = second;
+            second.next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+}
