8088e3eaa40a08ef6a923c275a71b4cf743c4661
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-17 16:30:33 -0400

    Add: linked-list-cycle-detection - submission-1

---
+++ Data Structures & Algorithms/linked-list-cycle-detection/submission-1.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode} head
+     * @return {boolean}
+     */
+    hasCycle(head) {
+        const visited = new Set();
+        let i = 0;
+        let curr = head;
+        while (curr) {
+            if (visited.has(curr.next))
+                return true;
+            visited.add(curr.next);
+            curr = curr.next;
+        }
+        return false;
+    }
+}
