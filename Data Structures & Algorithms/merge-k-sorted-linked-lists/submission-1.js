67d13e3baa096e2b3b7ff79c4e6003b48c8afec4
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-17 17:54:25 -0400

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     constructor(val = 0, next = null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {ListNode[]} lists
+     * @return {ListNode}
+     */
+    mergeKLists(lists) {
+        const dummyNode = new ListNode();
+        let curr = dummyNode;
+        while (true) {
+            let minIndex = -1;
+            let minNode = null;
+            for (let i = 0; i < lists.length; i++) {
+                if (!lists[i]) continue;
+                if (!minNode || lists[i].val < minNode.val) {
+                    minNode = lists[i];
+                    minIndex = i;
+                }
+            }
+            if (minIndex === -1) break;
+            lists[minIndex] = lists[minIndex].next;
+            curr.next = minNode;
+            curr = curr.next;
+        }
+        return dummyNode.next;
+    }
+}
