6f2a99afe17b467bd54e7ef300ade63f0dd77d82
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-17 11:39:47 -0400

    Add: string-encode-and-decode - submission-6

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-6.js
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {string[]} strs
+     * @returns {string}
+     */
+    encode(strs) {
+        return strs.map(s => `${s.length}#${s}`).join('');
+    }
+
+    /**
+     * @param {string} str
+     * @returns {string[]}
+     */
+    decode(str) {
+        let i = 0;
+        const words = [];
+        while (i < str.length) {
+            const j = str.indexOf('#', i);
+            const wordLen = parseInt(str.substring(i, j), 10);
+            const start = j + 1;
+            const end = start + wordLen;
+            words.push(str.substring(start, end));
+            i = end;
+        }
+        return words;
+    }
+}
