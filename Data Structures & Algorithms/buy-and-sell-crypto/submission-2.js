00fe7a8d11e9b95f28926f65c64417dde60be487
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-17 13:18:57 -0400

    Add: buy-and-sell-crypto - submission-2

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-2.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices) {
+        let minPrice = Infinity;
+        let maxProfit = 0;
+        
+        for (let i = 0; i < prices.length; i++) {
+            const price = prices[i];
+
+            if (price < minPrice) {
+                minPrice = price;
+            } else if (price - minPrice > maxProfit) {
+                maxProfit = price - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+}
