c8d17de09e87ddaf95be3106b87e09b775ee3b4e
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-19 21:53:23 -0400

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.js
@@ -0,0 +1,16 @@
+class Solution {
+    /**
+     * @param {number} m
+     * @param {number} n
+     * @return {number}
+     */
+    uniquePaths(m, n) {
+        const dp = new Array(m).fill(new Array(n).fill(1));
+        for (let r = 1; r < m; r++) {
+            for (let c = 1; c < n; c++) {
+                dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
