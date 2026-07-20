31c74f723e615db76e795ea8de1b4fbfc6e7bb26
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-19 23:34:46 -0400

    Add: meeting-schedule - submission-1

---
+++ Data Structures & Algorithms/meeting-schedule/submission-1.js
@@ -0,0 +1,27 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ *   constructor(start, end) {
+ *     this.start = start;
+ *     this.end = end;
+ *   }
+ * }
+ */
+
+class Solution {
+    /**
+     * @param {Interval[]} intervals
+     * @returns {boolean}
+     */
+    canAttendMeetings(intervals) {
+        if (!intervals.length) return true;
+        intervals.sort((a, b) => a.start - b.start);
+        let prevEnd = intervals[0].end;
+        for (let i = 1; i < intervals.length; i++) {
+            if (intervals[i].start < prevEnd)
+                return false;
+            prevEnd = intervals[i].end;
+        }
+        return true;
+    }
+}
