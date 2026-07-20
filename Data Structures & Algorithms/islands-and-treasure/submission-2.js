2d6ad2d936a21327a325bc433cf5db46d8425c90
Ramleton <77259553+Ramleton@users.noreply.github.com>
2026-07-20 16:40:01 -0400

    Add: islands-and-treasure - submission-2

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-2.js
@@ -0,0 +1,36 @@
+class Solution {
+    /**
+     * @param {number[][]} grid
+     */
+    islandsAndTreasure(grid) {
+        const rows = grid.length;
+        const cols = grid[0].length;
+
+        const queue = [];
+
+        for (let r = 0; r < rows; r++) {
+            for (let c = 0; c < cols; c++) {
+                if (grid[r][c] === 0) {
+                    queue.push([r, c]);
+                }
+            }
+        }
+
+        const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]];
+        let head = 0;
+
+        while (head < queue.length) {
+            const [r, c] = queue[head++];
+
+            for (const [dr, dc] of directions) {
+                const nr = r + dr;
+                const nc = c + dc;
+
+                if (nr >= 0 && nr < rows && nc >= 0 && nc < cols && grid[nr][nc] > grid[r][c] + 1) {
+                    grid[nr][nc] = grid[r][c] + 1;
+                    queue.push([nr, nc]);
+                }
+            }
+        }
+    }
+}
